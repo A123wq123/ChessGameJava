bcedf1acaedd945c1fbe462d55ea4ea1fe9f6bcf
A123wq123 <charles.degrandpre@polymtl.ca>
2022-08-24 20:23:46 -0400

    Initialise the Controller class

---
+++ ChessGame/src/ChessController.java
@@ -0,0 +1,6 @@
+// This will be the class in charge of controlling the game, this includes the player's turn, if the king is in check
+// and moving the pieces. Debating on if this class moves the pieces itself. 
+
+public class ChessController {
+
+}
