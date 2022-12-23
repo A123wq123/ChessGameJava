b3bb05951342cbe39ee7b9fddf6070d78f09c32f
A123wq123 <charles.degrandpre@polymtl.ca>
2022-12-23 13:01:36 -0500

    Added java doc to NullPiece class

--- ChessGame/src/Logic/Pieces/NullPiece.java
+++ ChessGame/src/Logic/Pieces/NullPiece.java
@@ -5,16 +5,29 @@ import Logic.Colour;
 
 import java.util.ArrayList;
 
+/**
+ * This class acts as a null piece, so a piece that does nothing. This class is
+ * usefully within the architecture of this project as all squares require pieces.
+ * @Author Charles Degrandpré
+ * @Last_Updated 2022-12-23
+ */
 public class NullPiece extends ChessABSPieceModel {
-
+    /**
+     * Constructor of a null piece. as the piece is meant to be
+     * unable to do anything, it doesn't have parameters.
+     */
     public NullPiece() {
-        // This class shouldn't really have parameters.
         this.colour = Colour.NULL;
     }
 
+    /**
+     * Returns the list of moves a null piece is capable of doing, which in
+     * this case is none.
+     * @param currentPos the current position of the piece.
+     * @return the list of moves the piece is capable of doing.
+     */
     @Override
     public ArrayList<ChessSquareModel> getListMoves(ChessSquareModel currentPos) {
-        // depending on how the code responds to the value being NULL, we might need to simply return an empty ArrayList
         return null;
     }
 }
