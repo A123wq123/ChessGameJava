83508c7ab1482fa2cd33d353426596f674d0daa4
A123wq123 <charles.degrandpre@polymtl.ca>
2022-12-23 11:40:06 -0500

    Added java documentation to Colour enum

--- ChessGame/src/Logic/Colour.java
+++ ChessGame/src/Logic/Colour.java
@@ -1,6 +1,11 @@
 package Logic;
 
-// types of pieces separated by color.
+/**
+ * Simple enum containing the different "teams" of pieces you can encounter.
+ * BLACK : a black coloured piece
+ * WHITE : a white coloured piece.
+ * NULL : an instance of the NullPiece class.
+ */
 public enum Colour {
     BLACK, WHITE, NULL
 }
