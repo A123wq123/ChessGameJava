b9fe704d9a9b819ed189681862a998cf56ae0fc4
A123wq123 <charles.degrandpre@polymtl.ca>
2022-12-23 13:38:03 -0500

    Added java doc to King class

--- ChessGame/src/Logic/Pieces/King.java
+++ ChessGame/src/Logic/Pieces/King.java
@@ -7,13 +7,31 @@ import Utility.Position;
 
 import java.util.ArrayList;
 
+/**
+ * The class representing a king within the project. This class is capable of returning all
+ * possible moves that the king can do.
+ * @Author Charles Degrandpré
+ * @Last_Updated 2022-12-23
+ */
 public class King extends ChessABSPieceModel {
 
+    /**
+     * Constructor of the King class. Assigns the board as well as the colour of the piece.
+     * @param board the instance of the ches board.
+     * @param colourOfPiece the colour of the king.
+     */
     public King(ChessBoardModel board, Colour colourOfPiece) {
         this.board = board;
         this.colour = colourOfPiece;
     }
 
+    /**
+     * Returns all the possibles moves the king can make by receiving its square
+     * as a parameter. This method does not check if a move is legal, it simply checks
+     * if the move is with the capabilities of the king.
+     * @param currentSquare
+     * @return an array of the possible squares the king can get to. 
+     */
     @Override
     public ArrayList<ChessSquareModel> getListMoves(ChessSquareModel currentSquare) {
 
