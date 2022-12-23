7060a8ccc6e2b4add6f6c0b6b79754e13a8cd3fd
A123wq123 <charles.degrandpre@polymtl.ca>
2022-12-23 11:42:42 -0500

    Added author of Colour enum

--- ChessGame/src/Logic/Colour.java
+++ ChessGame/src/Logic/Colour.java
@@ -5,6 +5,8 @@ package Logic;
  * BLACK : a black coloured piece
  * WHITE : a white coloured piece.
  * NULL : an instance of the NullPiece class.
+ * @Author Charles Degrandpré
+ * @Last_Updated 2022-12-23
  */
 public enum Colour {
     BLACK, WHITE, NULL
