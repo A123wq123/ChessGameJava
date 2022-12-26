22a2c65ef3098161505ec4d6151ba51f6a69401a
A123wq123 <charles.degrandpre@polymtl.ca>
2022-12-26 13:48:29 -0500

    Added test class for Position

---
+++ ChessGame/src/Test/src/ChessSquareUITest.java
@@ -0,0 +1,17 @@
+import UI.ChessSquareUI;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Series of automated tests for the ChessSquareUI class.
+ * @Author Charles Degrandpré
+ * @Last_Updated 2022-12-26
+ */
+public class ChessSquareUITest {
+
+    @Test
+    void constructSquareUI() {
+
+    }
+}
